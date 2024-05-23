a50cf9d0ba105cbf5881e248a4c101559d56359c
TatiYas <iasinovska_lawyer@ukr.net>
2024-05-23 14:36:06 +0200

    Update 2

--- js/task-6.js
+++ js/task-6.js
@@ -1,40 +1,41 @@
-const input = document.querySelector('input[type="number"]');
-const createBtn = document.querySelector('button[data-create]');
-const destroyBtn = document.querySelector('button[data-destroy]');
-const boxes = document.querySelector('#boxes');
-const controls = document.querySelector('#controls');
-
-createBtn.addEventListener('click', () => {
-  const amount = Number(input.value);
-  if (amount > 1 || amount < 100) {
-    destroyBoxes();
-    const boxesArray = [];
-    for (let i = 0; i < amount; i += 1) {
-      boxesArray.push(createBoxes(i));
-    }
-    input.value = '';
-    boxes.append(...boxesArray);
-  }
-});
-
-destroyBtn.addEventListener('click', () => {
-  destroyBoxes();
-});
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const input = document.querySelector('input');
+const container = document.querySelector('#boxes');
+
 function createBoxes(amount) {
-  const box = document.createElement('div');
-  box.style.width = `${30 + amount * 10}px`;
-  box.style.height = `${30 + amount * 10}px`;
-  box.style.backgroundColor = getRandomHexColor();
-  return box;
+  
+  container.innerHTML = '';
+  let size = 30;
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = size + 'px';
+    box.style.height = size + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+    size += 10;
+    boxes.push(box);
+  }
+
+  return container.append(...boxes);
 }
 
-function destroyBoxes() {
-  boxes.innerHTML = '';
-}
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(input.value);
+  input.value = '';
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+})
+
+destroyBtn.addEventListener('click', () => {
+  container.innerHTML = '';
+})
